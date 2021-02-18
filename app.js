7f430edc5940c1d4e13f22e8b82805e0d0242c29
Ellen Park <ellenpark@Ellens-MacBook-Pro.local>
2021-02-18 14:22:08 -0500

    creating an add and delete item function

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose")
 const app = express();
 
@@ -14,13 +13,13 @@ app.use(express.static("public"));
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
-mongoose.connect("mongodb://localhost:27017/todoDB", {useNewUrlParser: true})
+mongoose.connect("mongodb://localhost:27017/todoDB", {useNewUrlParser: true,  useUnifiedTopology: true })
 
 const itemsSchema = {
-  task: String,
+  name: String,
 }
 
-const Item = mongoose.model(Item, itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item ({
   name: "Welcome to your todo list!"
@@ -34,26 +33,48 @@ const item3 = new Item ({
   name: "<-- Hit this to delete an item"
 })
 
+const defaultItems = [item1, item2, item3];
+
 app.get("/", function(req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: "Today", newListItems: items});
+  Item.find({}, function(err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+        console.log("This has successfully added")
+        }
+      });
+      res.redirect("/")
+    } else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  })
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item = new Item ({
+    name: itemName
+  });
+  
+  item.save();
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  res.redirect("/")
 });
+
+app.post("/delete", function(req,res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function(err) {
+    if(!err) {
+      console.log("Item has been removed!")
+      res.redirect("/")
+    }
+  })
+})
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
